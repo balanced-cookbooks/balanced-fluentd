e115f0e2d24f0e52867311573bb90dd695d428aa
Marshall Jones <marshall@balancedpayments.com>
2014-08-28 15:53:54 -0700

    allow configuring the file log suffix

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dev@balancedpayments.com'
 license          'MIT'
 description      'Installs/Configures fluentd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.2'
 
 depends          'td-agent', '~> 0.0.1'
